dfe1bfb207c24998dc08e61765f497b099106ae5
Priyank Kodesia <priyankkodesia@gmail.com>
2017-10-06 05:27:42 +0530

    geolocation part1

--- public/js/index.js
+++ public/js/index.js
@@ -9,6 +9,7 @@
     		console.log("Disconnected from the server")
     	})
 
+
     	socket.on('newMessage',function(message){
     		console.log("Message recieved on the client side: ",message);
     		var username = message.from;
@@ -25,7 +26,28 @@
     		from : $('#username').val(),
     		text : $('#message').val()
     	}, function(){
+    		console.log("this got trigered")
+    	});
 
     	});
 
+    	var location_button = $('#location_button');
+    	location_button.on('click', function(){
+    		if(!navigator.geolocation){
+    			return alert('Geolocation not enabled on your device')
+    		}
+
+    		var coords = navigator.geolocation.getCurrentPosition(function(position){
+				var latitude = position.coords.latitude;
+				var longitude = position.coords.longitude;
+
+				return {latitude:latitude,longitude:longitude}
+
+	    		},function(err){
+    			console.log("Unable to fetch the location")
+    		})
     	})
+
+module.exports = {
+	coords
+}
