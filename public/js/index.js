c0d6ddf8651f0d37c9a59c8f6c2336dd432a38fe
Priyank Kodesia <priyankkodesia@gmail.com>
2017-10-06 05:57:46 +0530

    geolocation part1

--- public/js/index.js
+++ public/js/index.js
@@ -40,11 +40,43 @@
     		var cords = navigator.geolocation.getCurrentPosition(function(position){
 				var latitude = position.coords.latitude;
 				var longitude = position.coords.longitude;
-				console.log(latitude,longitude)
+})
+        var map, infoWindow;
+      function initMap() {
+        map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: -160.397, lng: 32.644},
+          zoom: 6
+        });
+        infoWindow = new google.maps.InfoWindow;
 
-	    		},function(err){
-    			console.log("Unable to fetch the location")
-    		})
+        // Try HTML5 geolocation.
+        if (navigator.geolocation) {
+          navigator.geolocation.getCurrentPosition(function(position) {
+            var pos = {
+              lat: position.coords.latitude,
+              lng: position.coords.longitude
+            };
+
+            infoWindow.setPosition(pos);
+            infoWindow.setContent('Location found.');
+            infoWindow.open(map);
+            map.setCenter(pos);
+          }, function() {
+            handleLocationError(true, infoWindow, map.getCenter());
+          });
+        } else {
+          // Browser doesn't support Geolocation
+          handleLocationError(false, infoWindow, map.getCenter());
+        }
+      }
+
+      function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+        infoWindow.setPosition(pos);
+        infoWindow.setContent(browserHasGeolocation ?
+                              'Error: The Geolocation service failed.' :
+                              'Error: Your browser doesn\'t support geolocation.');
+        infoWindow.open(map);
+      }
 
     	})
 
