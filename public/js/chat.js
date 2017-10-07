c0298f35240ef2014b936f2fed8805f1bda413db
Priyank Kodesia <priyankkodesia@gmail.com>
2017-10-07 05:57:08 +0530

    final commit

--- public/js/chat.js
+++ public/js/chat.js
@@ -37,7 +37,7 @@ socket.on('updateUserList', function (users) {
   var ol = jQuery('<ol></ol>');
 
   users.forEach(function (user) {
-    ol.append(jQuery('<li></li>').text(user));
+    ol.append(jQuery('<li style="font-size:20px" class="glyphicon glyphicon-play text-success"></li>').text(user));
   });
 
   jQuery('#users').html(ol);
@@ -75,7 +75,6 @@ jQuery('#message-form').on('submit', function (e) {
   var messageTextbox = jQuery('[name=message]');
 
   socket.emit('createMessage', {
-    from: 'User',
     text: messageTextbox.val()
   }, function () {
     messageTextbox.val('')
